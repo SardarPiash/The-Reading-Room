a6699ada2bd7c680ab9a1a5dbfb5decce7c0dc1c
Sardar Piash <piashm66@gmail.com>
2024-10-15 23:09:12 +0600

    card board fix

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 //default page
 const currentPage = 1;
+const authorIcon = 'assets/author.png';
+
 const bookStage = document.getElementById("book-lists");
 const paginationIndex = document.getElementById("paginationIndex")
 
@@ -11,28 +13,42 @@ async function fetchAPI(query='',page=1) {
      console.log(data.results)
     //  load data to display function
      displayBooks(data.results)
+     //console.log(authorIcon)
   }catch(error){
-
+    console.log(error)
   }
 };
 
 
-//book display function
-function displayBooks(books){
-    bookStage.innerHTML="";
-    books.forEach(book => {
-         const bookCard = document.createElement('div');
-        // bookCard.bookList.add("")
+function displayBooks(books) {
+  bookStage.innerHTML = ""; 
 
-         const bookImg = book.formats['image/jpeg'] || 'assets/fallback.jpg';
-         console.log("img",bookImg)
-        bookCard.innerHTML = `
-        <img src="${bookImg}" alt="Book Image" class="book-img" />
+  books.forEach(book => {
+      const bookCard = document.createElement('div');
+      bookCard.classList.add("book-card");
 
-        `;
-         bookStage.appendChild(bookCard);
-        console.log(bookCard)
-    });
+      const bookImg = book.formats['image/jpeg'] || 'assets/fallback.jpg';
+      console.log("img", bookImg);
+
+      bookCard.innerHTML = `
+          <div class="book-div">
+              <img src="${bookImg}" alt="Book Image" class="book-img" />
+          </div>
+          <div class="book-details">
+              <div style="font-size: 14px;">${book.title.slice(0,16)+(book.title.length>16 ? "...":null)}</div>
+              <div style="font-size: 10px; margin-top:2px;"><img src="${authorIcon}" class="author-icon"/>${book.authors[0].name.slice(0,16)+(book.authors[0]?.name.length>16 ? "...":null)}</div>
+              <div style="font-size: 14px;"><span style="font-weight: 400;">Language:</span>${book.languages}</div>
+              <div style="font-size: 14px;"><span style="font-weight: 400;">Download:</span>${book.download_count}</div>
+              <div style=" margin-top:10px;">
+              <button>View details</button>
+              <div>
+          </div>
+      `;
+
+      bookStage.appendChild(bookCard); 
+      console.log(bookCard);
+  });
+
     
 
 }
