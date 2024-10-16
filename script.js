4242ed0ed471b9be9edc81bfff8729a50019a36c
Sardar Piash <piashm66@gmail.com>
2024-10-17 01:22:43 +0600

    Homepage done

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ const bookStage = document.getElementById("book-lists");
 const paginationIndex = document.getElementById("paginationIndex");
 
 async function fetchAPI(query = "", page = 1) {
+  console.log("Home")
   try {
     const response = await fetch(
       `https://gutendex.com/books/?search=${query}&page=${page}`
     );
     const data = await response.json();
-    displayBooks(data.results);
-    console.log(data.results);
+    displayBooks(data.results,"all");
   } catch (error) {
     console.log(error);
   }
@@ -36,12 +36,12 @@ function handleWishList(id, iconElement) {
   localStorage.setItem("wishlistID", JSON.stringify(wislistArray));
 }
 
-function displayBooks(books) {
+function displayBooks(books,flag) {
   bookStage.innerHTML = "";
   books.forEach((book) => {
     const isInWishlist = wislistArray.includes(book.id);
     const icon = isInWishlist ? wishIconRed : wishIconBlack;
-
+   // const heading = `<span class="body-heading-text">Find your books</span>`;
     const bookCard = document.createElement("div");
     bookCard.classList.add("book-card");
 
@@ -85,7 +85,6 @@ function displayBooks(books) {
     wishlistButton.addEventListener("click", () => {
       handleWishList(book.id, iconElement);
     });
-
     bookStage.appendChild(bookCard);
   });
 }
@@ -109,4 +108,18 @@ function searchBooks(query) {
 //filter function
 function filterBooksByGenre(genre){
   fetchAPI(genre,currentPage);
+}
+
+//function to show wishlist 
+const showWishlist = async()=>{
+  console.log("cl")
+  const wishlist = JSON.parse(localStorage.getItem("wishlistID"))
+  const response = await fetch(
+    `https://gutendex.com//books?ids=${wishlist}`
+  );
+  const data = await response.json();
+  displayBooks(data.results,"wish");
+  console.log(data)
+
+
 }
